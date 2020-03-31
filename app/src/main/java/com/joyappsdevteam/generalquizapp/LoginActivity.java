e72f678e2d0679390c9ee44674c42ca5007f4583
JoykishanSharma <joykishan120@gmail.com>
2020-03-31 18:34:32 +0530

    Initial commit

---
+++ app/src/main/java/com/joyappsdevteam/generalquizapp/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.joyappsdevteam.generalquizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
